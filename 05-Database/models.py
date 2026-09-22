0ed97b7c82e52e81f1e4fd7c7c3b1d2ea23e742c
Shreyansh Kumar <shreyan.kumar29@gmail.com>
2026-09-22 18:51:44 +0530

    FastAPI

--- 05-Database/models.py
+++ 05-Database/models.py
@@ -25,9 +25,8 @@ class User(Base):
     @property
     def image_path(self) -> str:
         if self.image_file:
-            return f"/media/profile_pics/{self.image_file}"
+            return f"/static/profile_pics/{self.image_file}"
         return "/static/profile_pics/default.jpg"
-
 
 class Post(Base):
     __tablename__ = "posts"
